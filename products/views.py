c404a4e8c9144ba963e9c51a4c92921c17deed73
justin <neophilia85@gmail.com>
2020-07-11 23:58:58 +0200

    Making manually home.html and product_view

--- products/views.py
+++ products/views.py
@@ -1,11 +1,18 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
-products = models.Product.objects.all()
-
-
 def all_products(request):
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 4
+    limit = page_size * page
+    offset = limit - page_size
+    products = models.Product.objects.all()[offset:limit]
+    page_count = ceil(models.Product.objects.count() / page_size)
     return render(
-        request, template_name="product/home.html", context={"products": products}
+        request,
+        template_name="product/home.html",
+        context={"products": products, "page_count": page_count, "page": page},
     )
